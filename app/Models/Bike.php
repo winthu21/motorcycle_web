2e0d7db109e48885181c456f7fd94d01424669b4
Win Thu <mr.winthu21@gmail.com>
2024-11-26 23:47:02 +0700

    Add files via upload

--- app/Models/Bike.php
+++ app/Models/Bike.php
@@ -7,5 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Bike extends Model
 {
     //
-
+    protected $fillable = [
+        'bike_name',
+        'bike_image',
+        'brand_id',
+        'category_id',
+        'engine_power',
+        'model_year',
+        'bike_code_name',
+        'hp_torque',
+        'front_brake',
+        'rear_brake',
+        'wheel_size',
+        'fuel_system',
+        'suspension_front',
+        'suspension_rear',
+        'chain_type',
+        'swing_arm',
+        'display',
+        'headlight',
+        'frame',
+        'chassis',
+        'weight',
+        'starter',
+        'speed',
+    ];
 }
