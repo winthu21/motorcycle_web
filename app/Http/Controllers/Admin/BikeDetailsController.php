88f487526ea9c1b6ba4dafa4f6cb660afbdb717e
Win Thu <mr.winthu21@gmail.com>
2024-11-27 01:35:58 +0700

    Add files via upload

--- app/Http/Controllers/Admin/BikeDetailsController.php
+++ app/Http/Controllers/Admin/BikeDetailsController.php
@@ -2,18 +2,51 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class BikeDetailsController extends Controller
 {
     //bikeDetailsCreatePage
     public function bikeDetailsCreatePage(){
-        return view('admin.bikeDetails.create');
+        $company = Brand::get();
+        $category = Category::get();
+        return view('admin.bikeDetails.create',compact('company','category'));
     }
 
     //bikeDetails Create
     public function bikeDetailsCreate(Request $request){
+        $this->checkValidation($request);
         dd($request->toArray());
     }
+
+    //validation
+    private function checkValidation($request){
+        $rules = [
+            'bike_name' => 'required',
+            'bike_image' => 'required|file',
+            'brand_id'=> 'required',
+            'category_id'=> 'required',
+            'engine_power'=> 'required',
+            'model_year_from'=> 'required',
+            'model_year_to'=> 'required',
+            'hp_torque'=> 'required',
+            'front_brake'=> 'required',
+            'rear_brake'=> 'required',
+            'wheel_size'=> 'required',
+            'fuel_system'=> 'required',
+            'suspension_front'=> 'required',
+            'suspension_rear'=> 'required',
+            'chain_type'=> 'required',
+            'swing_arm'=> 'required',
+            'display'=> 'required',
+            'headlight'=> 'required',
+            'starter'=> 'required',
+            'speed'=> 'required',
+        ];
+        $validator = $request->validate($rules);
+    }
+
 }
